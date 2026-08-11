4b9cad7497951c725691677293b957ab44288a45
xonoxo143-ux <xonoxo143@gmail.com>
2026-08-11 19:27:39 -0400

    Add relation-subset composition holdout wrapper

---
+++ semzip/benchmarks/train_composition_subset.py
@@ -0,0 +1,6 @@
+from compiler_composition_corpus import generate_composition_examples
+import train_relation_subset_compiler as runner
+
+if __name__ == "__main__":
+    runner.generate_delta_examples = generate_composition_examples
+    runner.main()
