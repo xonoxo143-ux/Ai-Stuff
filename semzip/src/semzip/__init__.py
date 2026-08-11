bd8ab7be23e60337f226e87ce57af9a29192344e
xonoxo143-ux <xonoxo143@gmail.com>
2026-08-11 09:39:13 -0400

    Expose SemZip v0.3 semantic substrate

--- semzip/src/semzip/__init__.py
+++ semzip/src/semzip/__init__.py
@@ -3,17 +3,28 @@
 from .codec import SemZipCodec, UnsupportedMeaningError
 from .meaning import Meaning
 from .model import SemanticGraph
-from .story import MiniWorldInterpreter, UnsupportedStorySentence
+from .query import QueryAnswer, WorldQueryEngine
+from .schema import SemanticValidationError, validate_meaning
+from .story import (
+    AmbiguousReferenceError,
+    MiniWorldInterpreter,
+    UnsupportedStorySentence,
+)
 from .world import Ontology, WorldModel
 
 __all__ = [
+    "AmbiguousReferenceError",
     "Meaning",
     "MiniWorldInterpreter",
     "Ontology",
-    "SemZipCodec",
+    "QueryAnswer",
     "SemanticGraph",
+    "SemanticValidationError",
+    "SemZipCodec",
     "UnsupportedMeaningError",
     "UnsupportedStorySentence",
     "WorldModel",
+    "WorldQueryEngine",
+    "validate_meaning",
 ]
-__version__ = "0.2.0"
+__version__ = "0.3.0"
